4a511db3358cb1634480213594ac896f64cce3ac
jose maciel <jose.r.maciel@outlook.com>
2025-01-29 16:17:22 -0300

    melhoria na geração aleatória do mundo

--- src/ambiente.py
+++ src/ambiente.py
@@ -1,9 +1,9 @@
 import random
 
-from celula import Celula
+from sala import Sala
 
 class Ambiente:
-    def __init__(self, size:int=4, wumpus_qtd:int=1, poco_qtd:int=3, gold_qtd:int=1):
+    def __init__(self, size:int, wumpus_qtd:int=1, poco_qtd:int=3, gold_qtd:int=1):
         '''
         Objeto Ambiente será responsável guardar e processar os dados do mundo de wumpus.
         - size:int = Tamanho do mundo, o mundo sempre será de tamanho quadrado.
@@ -11,18 +11,35 @@ class Ambiente:
         - poco_qtd:int = é a quantidade de Poços no mundo.
         - gold_qtd:int = é a quantidade de Ouro no mundo.
         '''
-        self.__mundo = [
-            [Celula() for i in range(size) ] for i in range(size)
-        ] # Criação do mundo aplicado o objeto 'Celula' para cada posição.        
-
-        self.__size = size # Salva o tamanho do mundo no objeto.
         
-        for i in range(gold_qtd): 
-            self.__set_gold() # Define a posição aleatória do objeto Ouro e sua percepção.
-        for i in range(wumpus_qtd): 
-            self.__set_object('W', 'f') # Define a posição aleatória do objeto Wumpus e suas percepções.
+        # Criação do mundo aplicado o objeto 'Sala' para cada posição.
+        self.__mundo = [
+            [Sala() for i in range(size) ] for i in range(size)
+        ]
+        
+        self.__posicoes = {
+            'O':[],
+            'W':[],
+            'P':[]
+        }
+
+        # Salva o tamanho do mundo no objeto.
+        self.__size = size 
+        
         for i in range(poco_qtd): 
-            self.__set_object('P', 'b') # Define a posição aleatória do objeto Poço e suas percepções.
+            # Define a posição aleatória do objeto Poço.
+            pos = self.__set_object('P', 'b')
+            self.__posicoes['P'].append(pos) 
+
+        for i in range(wumpus_qtd): 
+            # Define a posição aleatória do objeto Wumpus.
+            self.__set_object('W', 'f') 
+            self.__posicoes['W'].append(pos) 
+
+        for i in range(gold_qtd): 
+            # Define a posição aleatória do objeto Ouro.
+            self.__set_gold() 
+            self.__posicoes['O'].append(pos) 
         
     def __str__(self): 
         '''
@@ -38,7 +55,7 @@ class Ambiente:
     
     def get_world(self) -> str:
         ''' 
-        Função que exibe o estado do mundo pela prioridade da celula.
+        Função que exibe o estado do mundo pela prioridade da sala.
         '''
         saida = ''
 
@@ -49,9 +66,9 @@ class Ambiente:
 
         return saida
     
-    def get_percepcoes(self): 
-        '''
-        Função que exibe apenas as percepções do mundo.
+    def get_percepcoes(self) -> str:
+        ''' 
+        Função que exibe asm percepções do mundo.
         '''
         saida = ''
 
@@ -61,59 +78,73 @@ class Ambiente:
             saida += ' |\n'
 
         return saida
-    
-    def __set_gold(self):
+            
+    def __set_gold(self) -> tuple[int, int]:
         '''
         Função que cálcula a posição aleatória do ouro e sua percepção.
         '''
-        pos_i, pos_j = 0, 0 # inicia as variáveis da posição
-        
         while True:
             pos_i = random.randint(0, self.__size - 1) # cálcula a posição i
             pos_j = random.randint(0, self.__size - 1)# cálcula a posição j
 
-            e_posicao_inicial = pos_i == 0 and pos_j == 0 # verifica se as posição cálculada é a inicial
-            posicao_esta_vazia = self.__mundo[pos_i][pos_j].objeto == ''# verifica se as posição cálculada está sem objetos
+            # verifica se as posição cálculada é a inicial
+            e_posicao_inicial = pos_i == 0 and pos_j == 0
 
-            if not e_posicao_inicial and posicao_esta_vazia:
-                break
-        
-        self.__mundo[pos_i][pos_j].objeto = 'O' # seta objeto 'O' para representar o ouro.
-        self.__mundo[pos_i][pos_j].percepcao += 'br' # senta a percepção br para representar o brilho
-        
-    def __set_object(self, objeto:str, percepcao:str):
+            # verifica se as posição cálculada está sem Poço
+            sala_sem_poco = 'P' not in self.__mundo[pos_i][pos_j].objeto
+
+            if not e_posicao_inicial and sala_sem_poco:
+                self.__mundo[pos_i][pos_j].objeto = 'O' # seta objeto 'O' para representar o ouro.
+                self.__mundo[pos_i][pos_j].percepcao = 'br' # senta a percepção br para representar o brilho
+
+                return pos_i, pos_j 
+
+    def __set_object(self, objeto:str, percepcao:str) -> tuple[int, int]:
         '''
         Função que cálcula a posição aleatória do ouro e sua percepção.
         '''
-        pos_i, pos_j = 0, 0
-        
         while True:
             pos_i = random.randint(0, self.__size - 1)
             pos_j = random.randint(0, self.__size - 1)
+            
+            # verifica se as posição cálculada é a inicial
+            e_posicao_inicial = pos_i == 0 and pos_j == 0
 
-            if pos_i != 0 and pos_j != 0 and self.__mundo[pos_i][pos_j].objeto == '':
-                break
-        
-        self.__mundo[pos_i][pos_j].objeto = objeto
-        
-        if pos_i + 1 < self.__size: # cálcula se existe a posição abaixo da cálculada
-            self.__mundo[pos_i + 1][pos_j].percepcao += percepcao
-            
-        if pos_j + 1 < self.__size: # cálcula se existe a posição a direita da cálculada
-            self.__mundo[pos_i][pos_j + 1].percepcao += percepcao
-            
-        if pos_i - 1 >= 0: # cálcula se existe a posição abaixo da cálculada
-            self.__mundo[pos_i - 1][pos_j].percepcao += percepcao
-            
-        if pos_j - 1 >= 0:# cálcula se existe a posição a esquerda da cálculada
-            self.__mundo[pos_i][pos_j - 1].percepcao += percepcao
+            # verifica se as posição cálculada está sem Poço
+            sala_sem_poco = self.__mundo[pos_i][pos_j].objeto != 'P'
 
+            if not e_posicao_inicial and sala_sem_poco:
+                # Salva o objeto na sala
+                self.__mundo[pos_i][pos_j].objeto = objeto
+                
+                # cálcula se existe a posição abaixo da cálculada
+                if pos_i + 1 < self.__size:
+                    if percepcao not in self.__mundo[pos_i + 1][pos_j].percepcao:
+                        self.__mundo[pos_i + 1][pos_j].percepcao = percepcao
+                    
+                # cálcula se existe a posição a direita da cálculada
+                if pos_j + 1 < self.__size: 
+                    if percepcao not in self.__mundo[pos_i][pos_j + 1].percepcao:
+                        self.__mundo[pos_i][pos_j + 1].percepcao = percepcao
+                    
+                # cálcula se existe a posição acima da cálculada
+                if pos_i - 1 >= 0:
+                    if percepcao not in self.__mundo[pos_i - 1][pos_j].percepcao:
+                        self.__mundo[pos_i - 1][pos_j].percepcao = percepcao
+                    
+                # cálcula se existe a posição a esquerda da cálculada
+                if pos_j - 1 >= 0:
+                    if percepcao not in self.__mundo[pos_i][pos_j - 1].percepcao:
+                        self.__mundo[pos_i][pos_j - 1].percepcao = percepcao
+                
+                return pos_i, pos_j
 
 
     
     
-if __name__ == '__main__': # funçao de testes do objeto
-    mapa = Ambiente()
+if __name__ == '__main__': 
+    # funçao de testes do objeto
+    mapa = Ambiente(3)
     print(mapa)
     print()
     print(mapa.get_percepcoes())
--- src/sala.py
+++ src/sala.py
@@ -1,14 +1,14 @@
-class Celula:
+class Sala:
     def __init__(self):
         '''
-        Célula que será usada para facilitar o armazenamento dos dados do "Mundo de Wumpus" sendo.
+        Sala que será usada para facilitar o armazenamento dos dados do "Mundo de Wumpus" sendo.
             - objeto:str = objeto presente na casa (Wumpus, Poço, ou Ouro).
             - percepcao:str = percepção que afeta a casa (fedor, brisa e brilho).
-            - caminho:str= parte da célula por onde o agente vai se mover sem sobreescrever os outos dados.
+            - caminho:str = parte da Sala por onde o agente vai se mover sem sobreescrever os outos dados.
         '''
-        self.__objeto = ''
-        self.__percepcao = ''
-        self.__caminho = ''
+        self.__objeto = '-'
+        self.__percepcao = '-'
+        self.__caminho = '-'
         self.__exibir = '-'
     
     @property
@@ -16,7 +16,7 @@ class Celula:
         '''
         Função responsável por exibir o dado de "maior prioridade" da céclula (Caminho > Objeto)
         '''
-        if self.__caminho != '':
+        if self.__caminho != '-':
             self.__exibir = self.__caminho
         else:
             self.__exibir = self.__objeto
@@ -29,7 +29,10 @@ class Celula:
     
     @objeto.setter
     def objeto(self, value: str):
-        self.__objeto = value
+        if self.__objeto == '-':
+            self.__objeto = value
+        else:
+            self.__objeto += value
     
     @property
     def caminho(self) -> str:
@@ -37,15 +40,18 @@ class Celula:
     
     @caminho.setter
     def caminho(self, value: str):
-        self.__caminho = value
-        
+        return self.__caminho
+
     @property
     def percepcao(self) -> str:
         return self.__percepcao
     
     @percepcao.setter
     def percepcao(self, value:str):
-        self.__percepcao = value
+        if self.__percepcao == '-':
+            self.__percepcao = value
+        else:
+            self.__percepcao += ',' + value
 
     def __str__(self) -> str:
         return self.exibir
@@ -54,7 +60,7 @@ if __name__ == '__main__':
     '''
     Teste de Funcionamento do Objeto
     '''
-    cell = Celula()
+    cell = Sala()
     cell.objeto = 'W'
     # cell.caminho = 'A'
     print(cell.exibir)
